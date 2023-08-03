0add77e62b8d8f96f3a698275f8619fbd3cd063d
sumanhong <suman0204@naver.com>
2023-08-03 18:02:13 +0900

    Day14 Assignment SearchBar Complete

--- SearchViewController.swift
+++ SearchViewController.swift
@@ -9,12 +9,37 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
-    @IBOutlet var searchViewLabel: UILabel!
+    
+    var movieInfo: MovieInfo = MovieInfo()
+//    {
+//        didSet {
+//            searchResultTableView.reloadData()
+//        }
+//    }
+    
+    var searchList:[Movie] = [] {
+        didSet{
+            searchResultTableView.reloadData()
+        }
+    }
+    
+    let searchBar = UISearchBar()
+    
+    @IBOutlet var searchResultTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        title = "검색화면"
+        
+//        title = "검색화면"
+        searchBar.delegate = self
+        searchBar.placeholder = "검색어를 입력해주세요"
+        searchBar.showsCancelButton = true
+        
+        //검색 결과 테이블 뷰 셀
+        searchResultTableView.delegate = self
+        searchResultTableView.dataSource = self
+        
+        navigationItem.titleView = searchBar
         
         let xmark = UIImage(systemName: "xmark")
         
@@ -22,9 +47,9 @@ class SearchViewController: UIViewController {
         
         navigationItem.leftBarButtonItem?.tintColor = .black
         
-        searchViewLabel.text = "검색화면"
-        searchViewLabel.textAlignment = .center
-        searchViewLabel.font = .boldSystemFont(ofSize: 30)
+        let nib = UINib(nibName: "AroundTableViewCell", bundle: nil)
+        searchResultTableView.register(nib, forCellReuseIdentifier: "AroundTableViewCell")
+        
     }
 
     @objc
@@ -34,3 +59,64 @@ class SearchViewController: UIViewController {
     }
 
 }
+
+
+extension SearchViewController: UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource{
+    
+    // searchResultTableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = searchResultTableView.dequeueReusableCell(withIdentifier: "AroundTableViewCell") as! AroundTableViewCell
+        
+        let row = searchList[indexPath.row]
+        
+        cell.configureCell(row: row)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+    
+    // searchBar
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchList.removeAll()
+        searchBar.text = ""
+        searchResultTableView.reloadData()
+        
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        searchList.removeAll()
+        
+        for movie in movieInfo.movieInfoList {
+            if movie.title.contains(searchBar.text!) {
+                searchList.append(movie)
+            }
+        }
+        
+        searchResultTableView.reloadData()
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        searchList.removeAll()
+        
+        for movie in movieInfo.movieInfoList {
+            if movie.title.contains(searchBar.text!) {
+                searchList.append(movie)
+            }
+        }
+        
+        searchResultTableView.reloadData()
+    }
+}
+
+
