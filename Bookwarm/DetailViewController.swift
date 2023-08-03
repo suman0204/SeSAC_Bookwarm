6721dc61a5394e97850270190199778484ff26f9
sumanhong <suman0204@naver.com>
2023-08-03 18:36:22 +0900

    Day14 Assignement TextView Complete

--- Bookwarm/DetailViewController.swift
+++ Bookwarm/DetailViewController.swift
@@ -18,13 +18,22 @@ class DetailViewController: UIViewController {
     @IBOutlet var movieOverviewTitleLabel: UILabel!
     @IBOutlet var movieOverviewLabel: UILabel!
     
+    @IBOutlet var memoTextView: UITextView!
+    
+    let placeholderText: String = "내용을 입력해주세요."
+
     var row: Movie = Movie(title: "정보없음", releaseDate: "정보없음", runtime: 0, overview: "정보없음", rate: 0.0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        memoTextView.text = contents
+//
+        memoTextView.text = placeholderText
+        memoTextView.textColor = .lightGray
         
-        
+        memoTextView.delegate = self
+
         configureDetailView(row: row)
         
     }
@@ -52,4 +61,24 @@ class DetailViewController: UIViewController {
     }
 
 
+}
+
+extension DetailViewController: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        print(#function)
+        if textView.text == placeholderText {
+            textView.text = nil
+            textView.textColor = .black
+        }
+    }
+
+    func textViewDidEndEditing(_ textView: UITextView) {
+        print(#function)
+        if textView.text.isEmpty {
+            textView.text = placeholderText
+            textView.textColor = .lightGray
+        }
+    }
+    
 }
